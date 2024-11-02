d1ee418c186fd543444e0b8c1731dbe80a1fd62e
Stephen <tpyngg@gmail.com>
2024-11-02 19:09:58 +0300

    added readme, requitment.txt, rechange config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
 # Telegram API credentials
-APP_ID = os.getenv('APP_ID', '21263303')
-APP_HASH = os.getenv('APP_HASH', 'a6f77a3037fbf0f739e0c462fe6c29c9')
-TOKEN = os.getenv('TOKEN', '7656686295:AAF3QURzzW4hX9tJewZU2UbKGN-Iszg1C2U')
+import os
+
+# Telegram API credentials
+APP_ID = os.getenv('APP_ID', 'your_app_id') 
+APP_HASH = os.getenv('APP_HASH', 'your_hash_id') 
+TOKEN = os.getenv('TOKEN', 'token_bkt') 
 
 # Optional configurations
-OWNER = os.getenv('OWNER', 'ivivigfj')  # For administrative commands
+OWNER = os.getenv('OWNER', 'owner_tag_without_@')  # For administrative commands 
